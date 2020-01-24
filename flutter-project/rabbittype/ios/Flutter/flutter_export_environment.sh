09f2492d92eb0a2ee1831bb78e04469f7c848138
chanathip2323 <chanathip.ru.60@ubu.ac.th>
2020-01-24 19:17:39 +0700

    born

---
+++ flutter-project/rabbittype/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/benz/Programs/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/benz/Desktop/rabbittype"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/benz/Programs/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
